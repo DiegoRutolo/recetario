d4bb60bbcae3568338a3a3d9b82539487d77c9ee
DiegoRutolo <diegorutolo@gmail.com>
2022-10-28 12:15:26 +0200

    Añadida navbar

--- src/main/java/eu/rutolo/recetario/recetas/RecetaController.java
+++ src/main/java/eu/rutolo/recetario/recetas/RecetaController.java
@@ -1,19 +1,12 @@
 package eu.rutolo.recetario.recetas;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/receta")
@@ -24,19 +17,6 @@ public class RecetaController {
     @Autowired
     private RecetaRepository recetaRepository;
     
-    @GetMapping
-    public @ResponseBody ResponseEntity<List<Receta>> list() {
-        List<Receta> recetas = recetaRepository.findAll();
-        logger.info(recetas.toString());
-        return new ResponseEntity<List<Receta>>(recetas, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<Receta> post(@RequestBody Receta receta) {
-        Receta result = recetaRepository.save(receta);
-        return new ResponseEntity<Receta>(result, HttpStatus.OK);
-    }
-
     @GetMapping("/ver")
     public String verRecetas(Model model) {
         model.addAttribute("recetas", recetaRepository.findAll());
