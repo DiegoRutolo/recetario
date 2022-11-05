8601833e69d4318d888a13389e037f65c75bfae4
DiegoRutolo <diegorutolo@gmail.com>
2022-11-05 16:46:10 +0100

    Ajustes menores

--- src/main/java/eu/rutolo/recetario/RecetarioApplication.java
+++ src/main/java/eu/rutolo/recetario/RecetarioApplication.java
@@ -1,42 +1,16 @@
 package eu.rutolo.recetario;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import eu.rutolo.recetario.recetas.Receta;
-import eu.rutolo.recetario.recetas.RecetaRepository;
-import eu.rutolo.recetario.security.users.Usuario;
-import eu.rutolo.recetario.security.users.UsuarioRepository;
 
 @SpringBootApplication
 public class RecetarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecetarioApplication.class, args);
-	}
-
-	@Bean
-	@Profile("ninguno")
-	public CommandLineRunner commandLineRunner(RecetaRepository recetaRepository, UsuarioRepository usuarioRepository, 
-			PasswordEncoder passwordEncoder) {
-		return args -> {
-			usuarioRepository.save(
-				new Usuario("admin", passwordEncoder.encode("admin"), 
-				true, true, true));
-			usuarioRepository.save(
-				new Usuario("user", passwordEncoder.encode("password"), 
-				true, true, false));
-
-			recetaRepository.save(new Receta(1l, "Patatas fritas", 
-				"Unas patatillas toh ricas", null, null));
-			recetaRepository.save(new Receta(2l, "Patatas cocidas", 
-				"Unas patatillas no tan ricas", null, null));
-		};
 	}
 
 	@Bean
--- src/main/java/eu/rutolo/recetario/recetas/RecetaController.java
+++ src/main/java/eu/rutolo/recetario/recetas/RecetaController.java
@@ -25,24 +25,24 @@ import eu.rutolo.recetario.security.users.Usuario;
 @Controller
 @RequestMapping("/receta")
 public class RecetaController {
-    private final Logger logger = LoggerFactory.getLogger(RecetaController.class);
+	private final Logger logger = LoggerFactory.getLogger(RecetaController.class);
 
-    @Autowired
-    private RecetaService recetaService;
-    
-    @GetMapping
-    public String listAll(Model model, @AuthenticationPrincipal Usuario usuario) {
-        model.addAttribute("recetas", recetaService.findByCreador(usuario));
-        return "recetas/recetasMain";
-    }
+	@Autowired
+	private RecetaService recetaService;
 
-    @GetMapping("/{id}")
+	@GetMapping
+	public String listAll(Model model, @AuthenticationPrincipal Usuario usuario) {
+		model.addAttribute("recetas", recetaService.findByCreador(usuario));
+		return "recetas/recetasMain";
+	}
+
+	@GetMapping("/{id}")
 	public String get(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("receta", recetaService.findById(id));
+		model.addAttribute("receta", recetaService.findById(id));
 		return "recetas/recetaForm";
 	}
 
-    @GetMapping(value = "/foto/{id}", produces = MediaType.IMAGE_PNG_VALUE)
+	@GetMapping(value = "/foto/{id}", produces = MediaType.IMAGE_PNG_VALUE)
 	public ResponseEntity<byte[]> getImage(@PathVariable("id") Long id) {
 		byte[] img = recetaService.findById(id).getFoto();
 		final HttpHeaders headers = new HttpHeaders();
@@ -50,12 +50,12 @@ public class RecetaController {
 		return new ResponseEntity<>(img, headers, HttpStatus.OK);
 	}
 
-    @GetMapping("/new")
+	@GetMapping("/new")
 	public String newRecetaGet(Receta receta, Model model) {
 		return "recetas/recetaForm";
 	}
 
-    @PostMapping
+	@PostMapping
 	public String newRecetaPost(Receta receta, @RequestParam("archivoFoto") MultipartFile file,
 			BindingResult result, Model model, @AuthenticationPrincipal Usuario usuario) throws IOException {
 		if (result.hasErrors()) {
@@ -66,24 +66,24 @@ public class RecetaController {
 		if (file != null && !file.isEmpty()) {
 			receta.setFoto(file.getBytes());
 		}
-        receta.setCreador(usuario);
+		receta.setCreador(usuario);
 		recetaService.save(receta);
 		return "redirect:/receta";
 	}
 
-    @GetMapping("/delete/{id}")
+	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable("id") Long id, Model model, @AuthenticationPrincipal Usuario usuario) {
 
-        try {
-            Receta r = recetaService.findById(id);
-            if (usuario.equals(r.getCreador()) || usuario.isRolAdmin()) {
-                recetaService.delete(r);
-            } else {
-                logger.info(usuario.getUsername() + " intenta borrar receta " + id + " sin permisos");
-            }
-        } catch (Exception e) {
-            logger.error("Error borrando receta " + id, e);
-        }
+		try {
+			Receta r = recetaService.findById(id);
+			if (usuario.equals(r.getCreador()) || usuario.isRolAdmin()) {
+				recetaService.delete(r);
+			} else {
+				logger.info(usuario.getUsername() + " intenta borrar receta " + id + " sin permisos");
+			}
+		} catch (Exception e) {
+			logger.error("Error borrando receta " + id, e);
+		}
 
 		return "redirect:/receta";
 	}
